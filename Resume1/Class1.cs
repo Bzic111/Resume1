9b65e70b9ac9adadcea56508f23fdfb0ecc37faa
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-12-27 01:16:45 +0300

    Добавьте файлы проекта.

---
+++ Resume1/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace Resume1;
+
+public class Circle
+{
+    public static double CalculateSquareByRadius(double r)
+    {
+        return Math.PI* Math.Pow(r,2);
+    }
+}
+public class Triangle
+{
+    public static bool IsRectangular(double a, double b, double c)
+    {
+        double[] temp = { a, b, c };
+        Array.Sort(temp);
+        return Math.Pow(temp[0], 2) + Math.Pow(temp[1], 2) == Math.Pow(temp[2], 2);            
+    }
+    public static double CalculateSquareByBorders(double a,double b, double c)
+    {
+        if (IsRectangular(a,b,c))
+        {
+            double[] temp = { a, b, c };
+            Array.Sort(temp);
+            return temp[0] * temp[1] / 2;
+        }
+        else
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Resume1;
+
+Console.WriteLine(Triangle.IsRectangular(3, 5, 4));
+Console.WriteLine(Triangle.IsRectangular(31, 51, 41));
+Console.WriteLine(Triangle.IsRectangular(6, 7, 8));
+Console.WriteLine(Triangle.IsRectangular(6, 10, 8));
+Console.WriteLine(Triangle.IsRectangular(32, 52, 42));
+
+Console.WriteLine(Triangle.CalculateSquareByBorders(5,4,3));
+Console.WriteLine(Triangle.CalculateSquareByBorders(31, 51, 41)); 
+Console.WriteLine(Triangle.CalculateSquareByBorders(6, 7, 8)); 
+Console.WriteLine(Triangle.CalculateSquareByBorders(6, 10, 8)); 
+Console.WriteLine(Triangle.CalculateSquareByBorders(52, 42, 32));
+
+Console.WriteLine(Circle.CalculateSquareByRadius(2));
+Console.WriteLine(Circle.CalculateSquareByRadius(3));
+Console.WriteLine(Circle.CalculateSquareByRadius(4));
+Console.WriteLine(Circle.CalculateSquareByRadius(5));
+Console.WriteLine(Circle.CalculateSquareByRadius(6));
